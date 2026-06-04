c3db076b63465887ef0cd7cfb678d64d5b958704
matiya-k <kimheonuk@hanmail.net>
2026-06-04 11:32:40 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // GitHub Pages 배포 시 레포 이름으로 변경
   // 예: https://username.github.io/jumping-stair-game/
-  base: '/jumping-stair-game/',
+  base: 'https://matiya-k.github.io/jumping-stair-game/',
 
   build: {
     outDir: 'dist',
